6457436dfef6cab2f97b2d3974f3dd0ec9deeee3
Nikara <nickolaslamin@gmail.com>
2026-04-04 23:38:15 -0300

    Implement CRUD operations for Destaque Positivo model and add database interaction methods

--- src/models/OcorrenciaModel.js
+++ src/models/OcorrenciaModel.js
@@ -13,7 +13,6 @@ export const findAll = async() =>{
     gravidade_id
     FROM ocorrencia
     `;
-
     try{
         const result = await pool.query(sql);
         return result.rows;
@@ -23,4 +22,93 @@ export const findAll = async() =>{
     }
 
 
-};
+}
+
+export const findIndex = async (id) => {
+    const sql = `
+    SELECT
+    id_ocorrencia,
+    datahora,
+    tipo_ocorrencia,
+    descricao,
+    acao_tomada,
+    registrado_por,
+    aluno_id,
+    gravidade_id
+    FROM ocorrencia
+    WHERE id_ocorrencia = $1
+    `;
+    try{
+        const result = await pool.query(sql,[id]);
+        return result.rows;
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
+}
+
+export const create = async (dataHora,tipoOcorrencia,descricao,acaoTomada,registroPor,alunoId,gravidadeId) =>{
+    const sql = `
+    INSERT INTO ocorrencia 
+    (
+        datahora,
+        tipo_ocorrencia,
+        descricao,
+        acao_tomada,
+        registrado_por,
+        aluno_id,
+        gravidade_id
+    )
+    VALUES
+    (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6,
+        $7
+    )
+        `;
+
+    try{
+        const result = await pool.query (sql, [dataHora,tipoOcorrencia,descricao,acaoTomada,registroPor,alunoId,gravidadeId]);
+        return result.rows;
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
+}
+
+export const update = async (id,tipoOcorrencia,descricao,acaoTomada) =>{
+     const sql = ` 
+    UPDATE ocorrencia 
+    SET  
+        tipo_ocorrencia = $1,
+        descricao = $2,
+        acao_tomada = $3,
+    WHERE id_ocorrencia = $4
+    `;
+
+    try {
+        const result = await pool.query(sql, [tipoOcorrencia,descricao,acaoTomada,id]);
+        return result.rows;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export const deletar = async(id) =>{
+    const sql = `
+    DELETE FROM ocorrencia
+    WHERE id_ocorrencia = $1
+    `;
+    try{
+        const result = await pool.query(sql,[id]);
+        return result.rows;
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
+}
