99f0d116c1ad2c58ca192064a2f4bf5a7cce3033
Nikara <nickolaslamin@gmail.com>
2026-04-21 21:48:21 -0300

    mudançãs

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,18 +1,33 @@
 import dotenv from 'dotenv';
 import pg from 'pg';
 
+import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-const {Pool} = pg;
-
-const pool = new Pool({
-  user: process.env.DB_USER,       // seu usuário
-  host: process.env.DB_HOST,      // ou IP do servidor
-  database: process.env.DB_NAME,        // nome do banco
-  password: process.env.DB_PASS,  // senha definida na instalação
-  port: process.env.DB_PORT,         
+const sequelize = new Sequelize(
+  process.env.DB_USER,
+  process.env.DB_NAME,
+  process.env.DB_PASS,
+{
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT
 })
 
-export default  pool;
+
+
+
+
+// const {Pool} = pg;
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,       // seu usuário
+//   host: process.env.DB_HOST,      // ou IP do servidor
+//   database: process.env.DB_NAME,        // nome do banco
+//   password: process.env.DB_PASS,  // senha definida na instalação
+//   port: process.env.DB_PORT,         
+// })
+
+export default  sequelize;
 
